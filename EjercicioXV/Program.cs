a6e26b11db24739a7d556e7723dd1f3513261d91
EmmanuelLeguizamon <ema.legui.carp@gmail.com>
2021-09-07 23:17:00 -0300

    Ejercicio 15

--- EjercicioXV/Program.cs
+++ EjercicioXV/Program.cs
@@ -6,7 +6,42 @@ namespace EjercicioXV
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese un número: ");
+
+            string prueba = Console.ReadLine();
+
+
+            if (Int64.TryParse(prueba, out Int64 CC7))
+            {
+                Console.WriteLine("Es un valor númerico");
+            }
+            else
+            {
+                if (Decimal.TryParse(prueba, out decimal CC8))
+                {
+                    Console.WriteLine("Es un valor númerico");
+                }
+                else
+                {
+                    if (long.TryParse(prueba, out long CC9))
+                    {
+                        Console.WriteLine("Es un valor númerico");
+                    }
+                    else
+                    {
+                        if (double.TryParse(prueba, out double CC10))
+                        {
+                            Console.WriteLine("Es un valor númerico");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("No es un valor númerico");
+                        }
+                    }
+                }
+            }
         }
+
     }
 }
